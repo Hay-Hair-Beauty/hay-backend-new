7342a2281775dabe6218f649ca86f814ece2d07c
febrianrizkiadisutiyo <12050116997@students.uin-suska.ac.id>
2024-06-18 16:31:16 +0700

    update

--- config/firebase.js
+++ config/firebase.js
@@ -1,19 +1,44 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json");
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 const { initializeApp } = require("firebase/app");
 const { getAnalytics } = require("firebase/analytics");
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  // credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://hay-hair-beauty.firebaseio.com",
-});
+const ENV = process.env.NODE_ENV || 'DEV';
 
-const db = admin.firestore();
+// Fungsi untuk mengakses secret dari Secret Manager
+// async function getSecret() {
+//   const client = new SecretManagerServiceClient();
+//   const secretName = 'projects/YOUR_PROJECT_ID/secrets/YOUR_SECRET_NAME/versions/latest';
+//   const [version] = await client.accessSecretVersion({ name: secretName });
+//   const payload = version.payload.data.toString('utf8');
+//   return JSON.parse(payload);
+// }
 
+// Fungsi untuk menginisialisasi Firebase Admin SDK
+async function initializeFirebaseAdmin() {
+  if (ENV === 'PROD') {
+    // Inisialisasi menggunakan akun layanan default
+    admin.initializeApp({
+      credential: admin.credential.applicationDefault(),
+      databaseURL: "https://hay-hair-beauty.firebaseio.com",
+    });
+  } else {
+    // Inisialisasi menggunakan file service account key
+    const serviceAccount = require("../serviceAccountKey.json");
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://hay-hair-beauty.firebaseio.com",
+    });
+  }
+
+  const db = admin.firestore();
+  return { admin, db };
+}
+
+// Konfigurasi Firebase SDK untuk analitik
 const firebaseConfig = {
   apiKey: "AIzaSyCXr2cwdIn-l2AKnVcz5eAGGhxQc3OGpeM",
   authDomain: "hay-hair-beauty.firebaseapp.com",
@@ -24,16 +49,23 @@ const firebaseConfig = {
   measurementId: "G-5QJHMN455P",
 };
 
-// Initialize Firebase
+// Inisialisasi Firebase SDK
 const app = initializeApp(firebaseConfig);
 
 // Check if the environment supports Firebase Analytics
+let analytics;
 if (typeof window !== "undefined" && typeof navigator !== "undefined") {
-  const analytics = getAnalytics(app);
+  analytics = getAnalytics(app);
 }
 
-module.exports = {
-  admin,
-  db,
-  app,
-};
+// Panggil fungsi untuk menginisialisasi Firebase Admin SDK
+initializeFirebaseAdmin()
+  .then(({ admin, db }) => {
+    module.exports = {
+      admin,
+      db,
+      app,
+      analytics,
+    };
+  })
+  .catch(console.error);
