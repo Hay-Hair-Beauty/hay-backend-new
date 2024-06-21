a684ec8a4957a1dfb2de2f34884f8d6aaf509f6e
febrianrizkiadisutiyo <12050116997@students.uin-suska.ac.id>
2024-06-21 08:26:13 +0700

    update

--- config/firestore.js
+++ config/firestore.js
@@ -1,10 +1,11 @@
 const { Firestore } = require('@google-cloud/firestore');
 const dotenv = require('dotenv');
+const serviceAccount = process.env.SERVICE_ACCOUNT ||require("../serviceAccountKey.json");
 
 dotenv.config();
 
 const firestore = new Firestore({
   projectId: process.env.GCLOUD_PROJECT_ID,
-  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+  keyFilename: serviceAccount
 });
 module.exports = firestore;
